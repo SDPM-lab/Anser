1293e1ba72f17cfe5f0f9cbb2629e57b2854b2e6
ping-yee <611077101@mail.nknu.edu.tw>
2022-12-13 00:07:53 +0800

    dev: the serialize repo implement.

---
+++ develop/app/Controllers/v1/Serialize.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controllers\V1;
+
+use App\Controllers\BaseController;
+use stdClass;
+use Zumba\JsonSerializer\JsonSerializer;
+
+class Serialize extends BaseController
+{
+    public function index()
+    {
+        $toBeSerialized = new stdClass();
+        $toBeSerialized->data = [1, 2, 3];
+        $toBeSerialized->name = 'N$ck';
+
+        // If the serialized object included the closure function, 
+        // you'll need to use the SuperClosure\Serializer repo.
+        // This interface is deprecated, waiting for the issue https://github.com/zumba/json-serializer/issues/49.
+        // $superClosure = new Serializer();
+
+        // If just need to serialize the object, 
+        // you just need to use Zumba\JsonSerializer\JsonSerializer repo.
+        // $jsonSerializer = new JsonSerializer($superClosure);
+        // $serialized = $jsonSerializer->serialize($toBeSerialized);
+        // $superClosure = new \SuperClosure\Serializer();
+        $jsonSerializer = new JsonSerializer();
+
+        $serialized = $jsonSerializer->serialize($toBeSerialized);
+        $unserialized = $jsonSerializer->unserialize($serialized);
+
+        var_dump($toBeSerialized == $unserialized);
+        var_dump($unserialized->data);
+        var_dump($unserialized->name);
+    }
+}
