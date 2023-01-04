c8208d844fc0fd84a1baa402c62aef844a1a07ac
ping-yee <611077101@mail.nknu.edu.tw>
2023-01-05 00:27:57 +0800

    dev: sample of user order orchestrator.

--- develop/app/Anser/Orchestrators/UserOrchestrator.php
+++ develop/app/Anser/Orchestrators/UserOrchestrator.php
@@ -3,7 +3,9 @@
 namespace App\Anser\Orchestrators;
 
 use SDPMlab\Anser\Orchestration\Orchestrator;
+use SDPMlab\Anser\Service\Action;
 use App\Anser\Services\UserService;
+use SDPMlab\Anser\Orchestration\Saga\Cache\CacheFactory;
 
 class UserOrchestrator extends Orchestrator
 {
@@ -17,11 +19,23 @@ class UserOrchestrator extends Orchestrator
 
     public function __construct()
     {
-        $this->userService = new UserService();   
+        $this->userService = new UserService();
     }
 
     protected function definition()
     {
+        $orderAction   = new Action("order_service", "GET", "/api/v1/order/1");
+        $paymentAction = new Action("payment_service", "GET", "/api/v1/payment/1");
+        $userAction    = new Action("user_service", "GET", "/api/v1/user");
+
+        $this->setCacheInstance(CacheFactory::initCacheDriver('redis', 'tcp://service_redis:6379'));
+        $this->setCacheOrchestratorKey("userOrder");
         
+        $this->setStep()
+            ->addAction("order", $orderAction);
+        $this->setStep()
+            ->addAction("payment", $paymentAction);
+        $this->setStep()
+            ->addAction("user", $userAction);
     }
 }
