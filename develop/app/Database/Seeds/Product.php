9206174b26f97441e5917c238b4c8493c02c49f3
蔡銘凱 <s18113223@stu.edu.tw>
2023-01-09 18:11:28 +0800

    dev: Fix data generated by product and wallet seeds,Add composer require mbezhanov/faker-provider-collection.

--- develop/app/Database/Seeds/Product.php
+++ develop/app/Database/Seeds/Product.php
@@ -4,17 +4,21 @@ namespace App\Database\Seeds;
 
 use CodeIgniter\Database\Seeder;
 use Faker\Factory;
+use Bezhanov\Faker\Provider\Commerce;
 
 class Product extends Seeder
 {
     public function run()
     {
-        $faker = Factory::create();
+        // $faker = Factory::create();
         $now   = date("Y-m-d H:i:s");
+
+        $faker = \Faker\Factory::create();
+        $faker->addProvider(new Commerce($faker));
 
         for ($i = 0; $i < 20; $i++) {
             $data = [
-                'name'       => $faker->words(1, true),
+                'name'       => $faker->productName,
                 'price'      => $faker->numberBetween(500, 1000),
                 'amount'     => $faker->numberBetween(10, 500),
                 'created_at' => $faker->dateTimeBetween('-2 month', '-1 days')->format('Y-m-d H:i:s'),
--- develop/app/Database/Seeds/Wallet.php
+++ develop/app/Database/Seeds/Wallet.php
@@ -3,23 +3,34 @@
 namespace App\Database\Seeds;
 
 use CodeIgniter\Database\Seeder;
-use Faker\Factory;
 
 class Wallet extends Seeder
 {
     public function run()
     {
-        $faker = Factory::create();
         $now   = date("Y-m-d H:i:s");
 
-        for ($i = 1; $i < 5; $i++) {
-            $data = [
-                'u_key'      => $i,
-                'balance'    => $faker->numberBetween(500, 100000),
-                'created_at' => $faker->dateTimeBetween('-2 month', '-1 days')->format('Y-m-d H:i:s'),
+        $data = [
+            [
+                'u_key'      => 1,
+                'balance'    => 50000000,
+                'created_at' => $now,
                 'updated_at' => $now,
-            ];
-            $this->db->table('wallet')->insert($data);
-        }
+            ],
+            [
+                'u_key'      => 2,
+                'balance'    => 50000000,
+                'created_at' => $now,
+                'updated_at' => $now,
+            ],
+            [
+                'u_key'      => 3,
+                'balance'    => 0,
+                'created_at' => $now,
+                'updated_at' => $now,
+            ],
+        ];
+
+        $this->db->table('wallet')->insertBatch($data);
     }
 }
