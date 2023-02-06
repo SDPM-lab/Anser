db98974ec5ee81662686c114f8a20e054aeeb836
ping-yee <611077101@mail.nknu.edu.tw>
2023-02-07 01:49:27 +0800

    Dev: start the `restarter` component design and implement. But this component has not yet been developed.

---
+++ src/Orchestration/Saga/Restarter/RestarterInterface.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace SDPMlab\Anser\Orchestration\Saga\Restarter;
+
+use SDPMlab\Anser\Orchestration\Saga\Cache\CacheHandlerInterface;
+
+interface RestarterInterface
+{
+    /**
+     * Get the cache instance.
+     *
+     * @param CacheHandlerInterface $cacheInstance
+     * @return RestarterInterface
+     */
+    public function setRestarterCacheInstance(CacheHandlerInterface $cacheInstance): RestarterInterface;
+
+    /**
+     * Re-start the interrupted runtime orchestrator.
+     *
+     * @param string|null $orchestratorNumber
+     * @return void
+     */
+    public function reStartOrchestrator(?string $orchestratorNumber);
+}
