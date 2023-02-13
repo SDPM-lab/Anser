0ab35810cb11e494e9982ad1abc6fe482a7f6269
ping-yee <611077101@mail.nknu.edu.tw>
2023-02-13 23:51:24 +0800

    Dev: Design the preliminary createOrder orchestator.

--- develop/app/Controllers/v2/CreateOrder.php
+++ develop/app/Controllers/v2/CreateOrder.php
@@ -5,16 +5,22 @@ namespace App\Controllers\V2;
 use App\Controllers\BaseController;
 use App\Anser\Orchestrators\V2\CreateOrderOrchestrator;
 use SDPMlab\Anser\Service\ServiceList;
+use App\Services\UserService;
 
 class CreateOrder extends BaseController
 {
     
     public function createOrder()
     {
+        $data = $this->request->getJSON(true);
+
+        $productsArray = $data["products"];
+        $userKey       = $this->request->getHeaderLine("X-User-Key");
+        
         $userOrch = new CreateOrderOrchestrator();
 
-        $result   = $userOrch->build();
+        $result   = $userOrch->build($productsArray, $userKey);
         
-        var_dump($result);
+        var_dump(json_decode($result));
     }
 }
