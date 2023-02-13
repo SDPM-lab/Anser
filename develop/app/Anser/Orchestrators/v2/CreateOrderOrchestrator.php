630e9f2ea7a17a5bb11aa7d0cb759d73802b85a2
ping-yee <611077101@mail.nknu.edu.tw>
2023-02-13 23:51:24 +0800

    Dev: Design the preliminary createOrder orchestator.

--- develop/app/Anser/Orchestrators/v2/CreateOrderOrchestrator.php
+++ develop/app/Anser/Orchestrators/v2/CreateOrderOrchestrator.php
@@ -4,7 +4,9 @@ namespace App\Anser\Orchestrators\V2;
 
 use SDPMlab\Anser\Orchestration\Orchestrator;
 use App\Anser\Services\V2\ProductService;
+use Exception;
 use SDPMlab\Anser\Orchestration\Saga\Cache\CacheFactory;
+use SDPMlab\Anser\Service\Action;
 
 class CreateOrderOrchestrator extends Orchestrator
 {
@@ -16,6 +18,26 @@ class CreateOrderOrchestrator extends Orchestrator
      */
     protected ProductService $productService;
 
+    /**
+     * Store the product info.
+     *
+     * @var array
+     */
+    protected array $productArray = [];
+
+    /**
+     * Store the product Actions.
+     *
+     * @var array
+     */
+    protected array $productActions = [];
+
+    protected $orderKey;
+
+    /**
+     * Cache instance.
+     *
+     */
     protected $cache;
 
     public function __construct()
@@ -23,17 +45,44 @@ class CreateOrderOrchestrator extends Orchestrator
         $this->productService = new ProductService();
     }
 
-    protected function definition()
+    protected function definition(array $productsArray = [], int $userKey = null)
     {
-        // $this->cache = CacheFactory::initCacheDriver('redis', 'tcp://anser_redis:6379');
+        if (empty($productsArray) || is_null($userKey)) {
+            throw new Exception("The parameters of product or userKey fail.");
+        }
 
-        // $this->setCacheInstance($this->cache);
-        // $this->setCacheOrchestratorKey('createOrder_1');
-        $this->setStep()->addAction("product_service", $this->productService->getAllProduct());
+        // Step 1. Check the product inventory balance.
+        $step1 = $this->setStep();
+        foreach ($productsArray as $key => $product_key) {
+            $actionName = "product_{$product_key}";
+
+            $this->productActions[] = $actionName;
+
+            $step1->addAction($actionName, $this->productService->getProduct($product_key));
+        }
+        
+        // Step 2. Check the user wallet balance.
+        // $step2 = $this->setStep()->addAction("wallet_check", new Action("payment_service", "GET"));
+        // Step 3. Create order.
+
+        // Step 4. Reduce the product inventory amount.
+
+        // Step 5. Reduce the user wallet balance.
+
     }
 
     protected function defineResult()
     {
-        return $this->isSuccess;
+        if ($this->isSuccess()) {
+            // foreach ($this->getStepAction("product_1")) {
+            // }
+            // return $this->getStepAction();
+
+            foreach ($this->productActions as $key => $actionName) {
+                $proudcts[] = $this->getStepAction($actionName)->getMeaningData();
+            }
+
+            return json_encode($proudcts);
+        }
     }
 }
