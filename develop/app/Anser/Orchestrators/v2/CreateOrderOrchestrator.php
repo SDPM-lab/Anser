0e21e83f645e4068603c8b9cdb1d24cf6120190c
ping-yee <611077101@mail.nknu.edu.tw>
2023-02-15 16:17:18 +0800

    Dev: Finish the orch step define. Not run yet.

--- develop/app/Anser/Orchestrators/v2/CreateOrderOrchestrator.php
+++ develop/app/Anser/Orchestrators/v2/CreateOrderOrchestrator.php
@@ -4,7 +4,10 @@ namespace App\Anser\Orchestrators\V2;
 
 use SDPMlab\Anser\Orchestration\Orchestrator;
 use App\Anser\Services\V2\ProductService;
+use App\Anser\Services\V2\PaymentService;
+use App\Anser\Services\V2\OrderService;
 use Exception;
+use SDPMlab\Anser\Orchestration\OrchestratorInterface;
 use SDPMlab\Anser\Orchestration\Saga\Cache\CacheFactory;
 use SDPMlab\Anser\Service\Action;
 
@@ -12,27 +15,39 @@ class CreateOrderOrchestrator extends Orchestrator
 {
 
     /**
-     * The sample of service.
+     * The service of product.
      *
-     * @var UserService
+     * @var ProductService
      */
     protected ProductService $productService;
 
     /**
-     * Store the product info.
+     * The service of payment.
      *
-     * @var array
+     * @var PaymentService
      */
-    protected array $productArray = [];
+    protected PaymentService $paymentService;
 
     /**
-     * Store the product Actions.
+     * The service of order.
+     *
+     * @var OrderService
+     */
+    protected OrderService $orderService;
+
+    /**
+     * The product information.
      *
      * @var array
      */
-    protected array $productActions = [];
+    protected array $product_data;
 
-    protected $orderKey;
+    /**
+     * The order key.
+     *
+     * @var integer
+     */
+    protected $order_key;
 
     /**
      * Cache instance.
@@ -43,46 +58,51 @@ class CreateOrderOrchestrator extends Orchestrator
     public function __construct()
     {
         $this->productService = new ProductService();
+        $this->paymentService = new PaymentService();
+        $this->orderService   = new OrderService();
     }
 
-    protected function definition(array $productsArray = [], int $userKey = null)
+    protected function definition(int $product_key = null, int $product_amout = null, int $user_key = null)
     {
-        if (empty($productsArray) || is_null($userKey)) {
-            throw new Exception("The parameters of product or userKey fail.");
+        if (empty($productsArray) || is_null($user_key)) {
+            throw new Exception("The parameters of product or user_key fail.");
         }
 
         // Step 1. Check the product inventory balance.
-        $step1 = $this->setStep();
-        foreach ($productsArray as $key => $product_key) {
-            $actionName = "product_{$product_key}";
+        $step1 = $this->setStep()->addAction("product_check", $this->productService->checkProductInventory($product_key, $product_amout));
 
-            $this->productActions[] = $actionName;
+        // Step 2. Get product info.
+        $step2 = $this->setStep()->addAction("get_product_info", $this->productService->getProduct($product_key));
+       
+        // Step 3. Check the user wallet balance.
+        $step3 = $this->setStep()->addAction("wallet_check", function (OrchestratorInterface $runtimeOrch) use ($user_key) {
+            $data = $runtimeOrch->getStepAction("get_product_info")->getMeaningData();
+            $this->product_data = $data["data"];
+            $this->paymentService->checkWalletBalance($user_key, $this->product_data["price"]);
+        });
 
-            $step1->addAction($actionName, $this->productService->getProduct($product_key));
-        }
-        
-        // Step 2. Check the user wallet balance.
-        // $step2 = $this->setStep()->addAction("wallet_check", new Action("payment_service", "GET"));
-        // Step 3. Create order.
+        // Step 4. Create order.
+        $step4 = $this->setStep()->addAction("create_order", $this->orderService->createOrder($user_key, $product_key, $product_amout, $this->product_data["price"] * $product_amout));
 
-        // Step 4. Reduce the product inventory amount.
+        // Step 5. Reduce the product inventory amount.
+        $step5 = $this->setStep()->addAction("reduce_product_amount", $this->productService->reduceInventory($product_key, $product_amout));
 
-        // Step 5. Reduce the user wallet balance.
-
+        // Step 6. Reduce the user wallet balance.
+        $step6 = $this->setStep()->addAction("reduce_wallet_balance", $this->paymentService->reduceWalletBalance($user_key, $this->product_data["price"] * $product_amout));
     }
 
     protected function defineResult()
     {
         if ($this->isSuccess()) {
-            // foreach ($this->getStepAction("product_1")) {
-            // }
-            // return $this->getStepAction();
-
-            foreach ($this->productActions as $key => $actionName) {
-                $proudcts[] = $this->getStepAction($actionName)->getMeaningData();
-            }
-
-            return json_encode($proudcts);
+            $order_data = $this->getStepAction("create_order")->getMeaningData();
+            $this->order_key = $order_data["data"]["orderID"];
+            
+            return json_encode([
+                "data" => [
+                    "status"       => true,
+                    "order_key"    => $this->order_key
+                ]
+            ]);
         }
     }
 }
