6930db9665d606f54e1e7d69f41fb8efe17ff827
ping-yee <611077101@mail.nknu.edu.tw>
2023-02-24 15:43:21 +0800

    Dev: The restarter logic have been implemented, but not test yet.

--- src/Orchestration/Saga/Restarter/RestarterInterface.php
+++ src/Orchestration/Saga/Restarter/RestarterInterface.php
@@ -5,21 +5,35 @@ namespace SDPMlab\Anser\Orchestration\Saga\Restarter;
 interface RestarterInterface
 {
     /**
-     * Re-start all pass in class name runtime orchestrators in Redis.
+     * Re-start all pass in class name and serverName runtime orchestrators in Redis.
      * If there is serverName list pass in, then Anser will restart all runtime orchestrator in Redis.
      * Otherwise, Anser will restart the all runtime orchestrators of this restarter setting server name.
      *
      * @param string|null $className
      * @param array|string $serverName
      * @param bool|null $isRestart If pass in true in this param, the restarter will restart the runtime orchestrator after compensation.
-     * Otherwise, The restarter will only run the compensation of this runtime orchestrator.
+     * Otherwise, The restarter will only run the compensation for this runtime orchestrator.
      * @param string|null $time
-     * @return bool $isSuccess Return whether re-start successfully.
+     * @return array
      */
-    public function reStartOrchestrator(
+    public function reStartOrchestratorsByServer(
         string $className = null,
         mixed $serverName = null,
-        ?bool $isRestart = false,
+        ?bool $isRestart  = false,
+        ?string $time = null
+    ): array;
+
+    /**
+     * Re-start all same className runtime orchestrator in Redis no matter what the serverName is.
+     *
+     * @param string|null $className
+     * @param boolean|null $isRestart
+     * @param string|null $time
+     * @return array
+     */
+    public function reStartOrchestratorsByClass(
+        string $className = null,
+        ?bool $isRestart  = false,
         ?string $time = null
     ): array;
 
